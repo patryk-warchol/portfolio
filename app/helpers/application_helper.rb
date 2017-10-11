a2171ed7e8ca98eedce79165eb6b0accf60f435f
patryk-warchol <warchol.patrick@gmail.com>
2017-10-11 21:32:47 +0200

    App interface

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def title(page_title = '')
+    base_title = "Patryk Warchol"
+    if page_title.empty?
+        base_title
+    else
+        page_title
+    end
+  end
+
 end
